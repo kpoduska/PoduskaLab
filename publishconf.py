0e223ec27ebfa36d8929ac8513662e5cf3936e70
ShivaniSemwal <49102687+ShivaniSemwal@users.noreply.github.com>
2024-04-08 11:00:16 -0230

    siteURLUpdate publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://kpoduska.github.io/PoduskaLab"
+SITEURL = "https://shivanisemwal.github.io/PoduskaLab_md/"
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
