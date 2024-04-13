ddc3779d74b67ca127c7d8acc475a437e5af9c29
kpoduska <46360720+kpoduska@users.noreply.github.com>
2024-04-13 13:20:43 -0230

    Update publishconf.py
    update siteurl to new repository name

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://kpoduska.github.io/PoduskaLabMD"
+SITEURL = "https://kpoduska.github.io/PoduskaLab"
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
