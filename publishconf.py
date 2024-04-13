37d36b9593daddffbd2e2cd9695249851710b0d2
kpoduska <46360720+kpoduska@users.noreply.github.com>
2024-04-13 13:08:11 -0230

    Update publishconf.py
    updated site URL to kpoduska from ShivaniSemwal

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://shivanisemwal.github.io/PoduskaLab_md"
+SITEURL = "https://kpoduska.github.io/PoduskaLabMD"
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
