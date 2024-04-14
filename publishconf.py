5beb8250816afc006baeb9b241bfd936b52a0199
kpoduska <46360720+kpoduska@users.noreply.github.com>
2024-04-14 15:12:02 -0230

    Update publishconf.py
    removed atom feed

--- publishconf.py
+++ publishconf.py
@@ -11,8 +11,8 @@ from pelicanconf import *
 SITEURL = " "
 RELATIVE_URLS = True
 
-FEED_ALL_ATOM = "feeds/all.atom.xml"
-CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
+#FEED_ALL_ATOM = "feeds/all.atom.xml"
+#CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
